11beec60dc2a8c554bba27a9dab76ddf4f6bd9db
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2024-08-30 21:44:45 +0300

    create user

--- backend/src/controllers/usersControllers.ts
+++ backend/src/controllers/usersControllers.ts
@@ -2,4 +2,8 @@ import { User } from "../entity/User";
 import { AppDataSource } from "../data-source";
 
 const user = new User()
-user.firstName
+async function createuser(){
+    user.firstName
+    user.lastName
+    
+}
